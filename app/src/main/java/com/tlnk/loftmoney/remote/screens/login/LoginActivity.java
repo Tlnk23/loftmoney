4cc3f7de2b1609d3320c766a4c7f29d54b36e78a
Tlnk23 <52453198+Tlnk23@users.noreply.github.com>
2020-11-05 22:53:32 +0300

    begin

---
+++ app/src/main/java/com/tlnk/loftmoney/remote/screens/login/LoginActivity.java
@@ -0,0 +1,7 @@
+package com.tlnk.loftmoney.remote.screens.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/tlnk/loftmoney/remote/screens/splash/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.tlnk.loftmoney.remote.screens.splash;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
